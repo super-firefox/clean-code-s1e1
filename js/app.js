fe2fce6db8e2e14ae401a925bd879a720daa57e5
Ihor RSS <superfirfox@gmail.com>
2021-11-27 21:27:06 +0200

    fix: path to svg in app.js

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ var createNewTaskElement = function (taskString) {
   editButton.className = "edit";
 
   deleteButton.className = "delete";
-  deleteButtonImg.src = './remove.svg';
+  deleteButtonImg.src = '../assets/remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
 
